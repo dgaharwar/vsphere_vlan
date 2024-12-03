7039a8560bfcbc87a8b2b139a1fcdc3ffe8aec47
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-12-03 16:33:09 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,8 +4,8 @@
 
 locals {
   vlan_id = tonumber(var.vlan_id)
-  begin_ip = "${local.vlan_id}" == "901" ? "10.180.25.2" : "${local.vlan_id}" == "902" ? "10.180.26.2" : "${local.vlan_id}" == "903" ? "10.180.27.2" : "${local.vlan_id}" == "904" ? "10.180.28.2" : "${local.vlan_id}" == "905" ? "10.180.29.2" : "${local.vlan_id}" == "906" ? "10.180.30.2" :  "${local.vlan_id}" == "907" ? "10.180.31.2" : "${local.vlan_id}" == "908" ? "10.180.32.2" : "${local.vlan_id}" == "909" ? "10.180.33.2" : "${local.vlan_id}" == "910" ? "10.180.34.2" : "${local.vlan_id}" == "911" ? "10.180.35.2" : "${local.vlan_id}" == "912" ? "10.180.36.2" : "null"
-  end_ip = "${local.vlan_id}" == "901" ? "10.180.25.254" : "${local.vlan_id}" == "902" ? "10.180.26.254" : "${local.vlan_id}" == "903" ? "10.180.27.254" : "${local.vlan_id}" == "904" ? "10.180.28.254" : "${local.vlan_id}" == "905" ? "10.180.29.254" : "${local.vlan_id}" == "906" ? "10.180.30.254" :  "${local.vlan_id}" == "907" ? "10.180.31.254" : "${local.vlan_id}" == "908" ? "10.180.32.254" : "${local.vlan_id}" == "909" ? "10.180.33.254" : "${local.vlan_id}" == "910" ? "10.180.34.254" : "${local.vlan_id}" == "911" ? "10.180.35.254" : "${local.vlan_id}" == "912" ? "10.180.36.254" : "null"
+  begin_ip = "${local.vlan_id}" == 901 ? "10.180.25.2" : "${local.vlan_id}" == 902 ? "10.180.26.2" : "${local.vlan_id}" == 903 ? "10.180.27.2" : "null"
+  end_ip = "${local.vlan_id}" == 901 ? "10.180.25.254" : "${local.vlan_id}" == 902 ? "10.180.26.254" : "${local.vlan_id}" == 903 ? "10.180.27.254" : "null"
 }
 
 variable "vsphere_provider_user"{
