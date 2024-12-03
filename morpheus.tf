a000a7f7ad2f513ddfb4b43c9de53027ef4d4d56
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-12-03 14:54:36 +0000

    Update morpheus.tf

--- morpheus.tf
+++ morpheus.tf
@@ -2,14 +2,6 @@
 #          Resources  - Morpheus                 #
 ##################################################
 
-#resource "morpheus_ipv4_ip_pool" "vlan_ip_pool" {
-#    name = "Vlan${var.vlan_id}-ip-pool"
-#    ip_range {
-#    starting_address = local.begin_ip
-#    ending_address   = local.end_ip
-#  }
-#}
-
 resource "morpheus_ipv4_ip_pool" "vlan_ip_pool" {
     name = "Vlan${local.vlan_id}-ip-pool"
     ip_range {
