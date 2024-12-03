22400df2fb8c37c20d116b941ef9b4bed4de7d3c
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2024-12-03 14:53:00 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,8 +4,6 @@
 
 locals {
   vlan_id = tonumber(var.vlan_id)
-#  begin_ip = "${var.vlan_id}" == "901" ? "10.180.25.2" : "${var.vlan_id}" == "902" ? "10.180.26.2" : "${var.vlan_id}" == "903" ? "10.180.27.2" : "${var.vlan_id}" == "904" ? "10.180.28.2" : "${var.vlan_id}" == "905" ? "10.180.29.2" : "${var.vlan_id}" == "906" ? "10.180.30.2" :  "${var.vlan_id}" == "907" ? "10.180.31.2" : "${var.vlan_id}" == "908" ? "10.180.32.2" : "${var.vlan_id}" == "909" ? "10.180.33.2" : "${var.vlan_id}" == "910" ? "10.180.34.2" : "${var.vlan_id}" == "911" ? "10.180.35.2" : "${var.vlan_id}" == "912" ? "10.180.36.2" : "null"
-#  end_ip = "${var.vlan_id}" == "901" ? "10.180.25.254" : "${var.vlan_id}" == "902" ? "10.180.26.254" : "${var.vlan_id}" == "903" ? "10.180.27.254" : "${var.vlan_id}" == "904" ? "10.180.28.254" : "${var.vlan_id}" == "905" ? "10.180.29.254" : "${var.vlan_id}" == "906" ? "10.180.30.254" :  "${var.vlan_id}" == "907" ? "10.180.31.254" : "${var.vlan_id}" == "908" ? "10.180.32.254" : "${var.vlan_id}" == "909" ? "10.180.33.254" : "${var.vlan_id}" == "910" ? "10.180.34.254" : "${var.vlan_id}" == "911" ? "10.180.35.254" : "${var.vlan_id}" == "912" ? "10.180.36.254" : "null"
   begin_ip = "${local.vlan_id}" == "901" ? "10.180.25.2" : "${local.vlan_id}" == "902" ? "10.180.26.2" : "${local.vlan_id}" == "903" ? "10.180.27.2" : "${local.vlan_id}" == "904" ? "10.180.28.2" : "${local.vlan_id}" == "905" ? "10.180.29.2" : "${local.vlan_id}" == "906" ? "10.180.30.2" :  "${local.vlan_id}" == "907" ? "10.180.31.2" : "${local.vlan_id}" == "908" ? "10.180.32.2" : "${local.vlan_id}" == "909" ? "10.180.33.2" : "${local.vlan_id}" == "910" ? "10.180.34.2" : "${local.vlan_id}" == "911" ? "10.180.35.2" : "${local.vlan_id}" == "912" ? "10.180.36.2" : "null"
   end_ip = "${local.vlan_id}" == "901" ? "10.180.25.254" : "${local.vlan_id}" == "902" ? "10.180.26.254" : "${local.vlan_id}" == "903" ? "10.180.27.254" : "${local.vlan_id}" == "904" ? "10.180.28.254" : "${local.vlan_id}" == "905" ? "10.180.29.254" : "${local.vlan_id}" == "906" ? "10.180.30.254" :  "${local.vlan_id}" == "907" ? "10.180.31.254" : "${local.vlan_id}" == "908" ? "10.180.32.254" : "${local.vlan_id}" == "909" ? "10.180.33.254" : "${local.vlan_id}" == "910" ? "10.180.34.254" : "${local.vlan_id}" == "911" ? "10.180.35.254" : "${local.vlan_id}" == "912" ? "10.180.36.254" : "null"
 }
@@ -16,15 +14,8 @@ variable "vsphere_provider_user"{
 
 variable "vsphere_provider_password"{
   type        = string
+  sensitive   = true
 }
-
-#variable "vsphere_m_cloud_user"{
-#  type        = string
-#}
-
-#variable "vsphere_m_cloud_password"{
-#  type        = string
-#}
 
 variable "vsphere_ip"{
   type        = string
@@ -38,6 +29,7 @@ variable "morpheus_url" {
 variable "morpheus_password" {
   description = "Morpheus Data user password"
   type       = string
+  sensitive  = true
 }
 
 variable "morpheus_username" {
@@ -48,6 +40,7 @@ variable "morpheus_username" {
 variable "vlan_id" {
   type = string
 }
+
 ##################################################
 #               Data Sources                     #
 ##################################################
